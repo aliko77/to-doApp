66e884497ea6deb9ae911d00be0ccc489d0adfb5
aliko77 <akoc3273@gmail.com>
2023-02-23 00:58:49 +0300

    Login

--- todoProject/urls.py
+++ todoProject/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('apps.main.urls'))
 ]
