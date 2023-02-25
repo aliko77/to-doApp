d13e01a4cd4efba19fd3292154e329774f2db343
alikoc <akoc3273@gmail.com>
2023-02-25 19:46:49 +0300

    update

--- todoProject/urls.py
+++ todoProject/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("__reload__/", include("django_browser_reload.urls")),
+    path('__reload__/', include('django_browser_reload.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path("account/", include('apps.register.urls')),
+    path('accounts/', include('apps.register.urls')),
     path('', include('apps.main.urls'))
 ]
