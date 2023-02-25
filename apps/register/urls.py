ba1bca07cde5ff6404fe5aae887ab9f26014885c
alikoc <akoc3273@gmail.com>
2023-02-25 19:46:41 +0300

    register

--- apps/register/urls.py
+++ apps/register/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from apps.register.views import register
+from apps.register import views
 
 urlpatterns = [
-    path('register', register.as_view(), name='register')
+    path('register/', views.register.as_view(), name='register')
 ]
--- apps/register/views.py
+++ apps/register/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-from django.http import HttpResponse
+from django.contrib import messages
+from .forms import RegisterForm
 
 # Create your views here.
 
 
 class register(View):
+    def post(self, request):
+        """
+        redirect guest users to home page
+        """
+        if request.user.is_authenticated:
+            return redirect('home')
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(
+                request=request, message='Başarıyla kayıt oldunuz.')
+            return redirect('login')
+        else:
+            return render(request, 'register/register.html', {'form': form})
+
     def get(self, request):
-        return HttpResponse('hi')
+        """
+        redirect guest users to home page
+        """
+        if request.user.is_authenticated:
+            return redirect('home')
+        return render(request=request, template_name='register/register.html')
