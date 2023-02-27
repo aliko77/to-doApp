2737554492360166a9b743680bf8403cf9e51325
alikoc <akoc3273@gmail.com>
2023-02-27 11:17:00 +0300

    .

--- apps/main/static/main/js/script.js
+++ apps/main/static/main/js/script.js
@@ -1,14 +1,13 @@
 "use strict";
 
-document
-    .querySelectorAll("[data-display-toggle]")
-    .forEach(function ($triggerEl) {
-        var drawerId = $triggerEl.getAttribute("data-display-toggle");
-        var $drawerEl = document.getElementById(drawerId);
-        if ($drawerEl) {
-            $triggerEl.addEventListener('click', function () {
-                console.log('hi');
-                $drawerEl.classList.toggle('-translate-x-full')
-            });
-        }
-    });
+// document
+//     .querySelectorAll("[data-display-toggle]")
+//     .forEach(function ($triggerEl) {
+//         var drawerId = $triggerEl.getAttribute("data-display-toggle");
+//         var $drawerEl = document.getElementById(drawerId);
+//         if ($drawerEl) {
+//             $triggerEl.addEventListener('click', function () {
+//                 $drawerEl.classList.toggle('-translate-x-full')
+//             });
+//         }
+//     });
---
+++ apps/tasks/static/tasks/js/tasks.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const sidebarButton = document.getElementById('sidebar-button');
+const buttonTarget = sidebarButton.getAttribute('aria-controls');
+const sidebar = document.getElementById(buttonTarget);
+if (sidebar) {
+    
+}
