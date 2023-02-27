8da3ce7bd3d27f67ffa2f214836e699f75759c2b
alikoc <akoc3273@gmail.com>
2023-02-27 12:41:39 +0300

    off

--- apps/tasks/static/tasks/js/tasks.js
+++ apps/tasks/static/tasks/js/tasks.js
@@ -1,8 +1,11 @@
 "use strict";
 
 const sidebarButton = document.getElementById('sidebar-button');
-const buttonTarget = sidebarButton.getAttribute('aria-controls');
-const sidebar = document.getElementById(buttonTarget);
-if (sidebar) {
-    
-}
+
+sidebarButton.addEventListener("click", () => {
+    const buttonTarget = sidebarButton.getAttribute('aria-controls');
+    const sidebar = document.getElementById(buttonTarget);
+    if (sidebar) {
+        sidebar.classList.toggle('-translate-x-full')
+    }
+});
