7da261891355acfbacafe5c67fbb87ba9763801f
alikoc <akoc3273@gmail.com>
2023-03-01 19:48:48 +0300

    tasks

--- apps/tasks/static/tasks/js/tasks.js
+++ apps/tasks/static/tasks/js/tasks.js
@@ -1,13 +1,22 @@
 "use strict";
 
-// const sidebarButton = document
-//     .querySelectorAll("#sidebar-button")
-//     .forEach(function ($triggerEl) {
-//         const buttonTarget = $triggerEl.getAttribute("aria-controls");
-//         const targetEl = document.getElementById(buttonTarget);
-//         if (targetEl) {
-//             $triggerEl.addEventListener("click", function () {
+var addMissionInput = document.getElementById('mission-input');
+var addMissionButton = document.getElementById('mission-add-button');
 
-//             });
-//         }
-//     });
+addMissionInput.addEventListener("keyup", (event) => {
+    const result = event.target.value
+    if (result.length > 0) {
+        try {
+            if (addMissionButton.disabled)
+                addMissionButton.disabled = false
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        try {
+            addMissionButton.disabled = true
+        } catch (error) {
+            console.log(error);
+        }
+    }
+});
