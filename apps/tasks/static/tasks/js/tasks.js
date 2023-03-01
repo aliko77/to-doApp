491d2152e82d8a68da7d4f1f967411a4272549ec
alikoc <akoc3273@gmail.com>
2023-03-01 12:26:34 +0300

    hihi

--- apps/tasks/static/tasks/js/tasks.js
+++ apps/tasks/static/tasks/js/tasks.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const sidebarButton = document
-    .querySelectorAll("#sidebar-button")
-    .forEach(function ($triggerEl) {
-        const buttonTarget = $triggerEl.getAttribute("aria-controls");
-        const targetEl = document.getElementById(buttonTarget);
-        if (targetEl) {
-            $triggerEl.addEventListener("click", function () {
+// const sidebarButton = document
+//     .querySelectorAll("#sidebar-button")
+//     .forEach(function ($triggerEl) {
+//         const buttonTarget = $triggerEl.getAttribute("aria-controls");
+//         const targetEl = document.getElementById(buttonTarget);
+//         if (targetEl) {
+//             $triggerEl.addEventListener("click", function () {
 
-            });
-        }
-    });
+//             });
+//         }
+//     });
