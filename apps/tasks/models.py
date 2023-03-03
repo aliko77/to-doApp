fcb05d205b09e52aff7bf3dd80b97050aebcca7c
alikoc <akoc3273@gmail.com>
2023-03-03 12:31:34 +0300

    tasks model, create migrate, add admin

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -5,8 +5,16 @@ from django.db import models
 
 class UserTasks(models.Model):
     user_id = models.IntegerField()
-    content = models.CharField(max_length=255)
-    expiration_date = models.DateField()
+    header = models.CharField(max_length=255)
+    description = models.TextField()
+    state = models.CharField(max_length=32)
+    expiration_date = models.DateField(null=True)
+    is_important = models.BooleanField(default=False)
+    step_id = models.BigIntegerField(default=0)
+    categories_id = models.BigIntegerField(default=0)
+    files_id = models.BigIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return "Görev: " + self.header
