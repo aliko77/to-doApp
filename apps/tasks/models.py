a7d9a4347eb3f1a409e680d0a231fbd67de24a43
aliko77 <akoc3273@gmail.com>
2023-03-03 10:28:06 +0300

    .

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserTasks(models.Model):
+    user_id = models.IntegerField()
+    content = models.CharField(max_length=255)
+    expiration_date = models.DateField()
+
+    def __str__(self):
+        return self.name
